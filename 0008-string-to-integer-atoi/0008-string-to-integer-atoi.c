1b48de86d9fbfc5825852152394798b02e92385d
azaz0101 <52024666+azaz0101@users.noreply.github.com>
2024-11-06 13:32:39 +0800

    Time: 0 ms (100%), Space: 8.7 MB (31.71%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.c
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.c
@@ -1,43 +1,25 @@
 int myAtoi(char* s) {
     long long int ans=0;
-    char *p=s;
+    int sign=1;
     int i;
-    if (*s == NULL){
-        return 0;
+    while (isspace(s[i])){
+        i++;
     }
-    ans = 0;
-    p = strtok(s, " ");
-    if (p == NULL){
-        return 0;
-    }
-    if (!isdigit(p[0]) && p[0] != '-' && p[0] != '+'){
-        return 0;
-    }
-    int len = strlen(p);
-    if (p[0] == '-' || p[0] == '+'){
-        i = 1;
-    }
-    else {
-        i = 0;
-    }
-    for (i;i<len;i++){
-        if (isdigit(p[i])){
-            ans = ans * 10 + (p[i] - '0');
-            if ((ans > INT_MAX) && (p[0] != '-')){
-                ans = INT_MAX;
-                break;
-            }
-            else if (p[0] == '-' && (ans - 1 > INT_MAX)){
-                ans = INT_MIN;
-                break;
-            }
+    if (s[i] == '-' || s[i] == '+'){
+        if (s[i] == '-'){
+            sign = -1;
         }
-        else {
-            break;
+        i++;
+    }
+    while (isdigit(s[i])){
+        ans = ans * 10 + (s[i] - '0');
+        if (sign == 1 && ans > INT_MAX){
+            return INT_MAX;
         }
+        else if (sign == -1 && -ans < INT_MIN){
+            return INT_MIN;
+        }
+        i++;
     }
-    if (p[0] == '-' && ans != INT_MIN){
-        ans *= -1; 
-    }
-    return ans;
+    return sign * ans;
 }
