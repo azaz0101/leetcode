77e4bd92a03fa64a895c2a3fab8820374f4105dd
azaz0101 <52024666+azaz0101@users.noreply.github.com>
2024-12-06 11:47:19 +0800

    Time: 28 ms (26.48%), Space: 14.2 MB (10.84%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int diameter=0;
+
+int tree_height(struct TreeNode *root){
+    if (root == NULL){
+        return 0;
+    }
+    else {
+        int l_h, r_h;
+        l_h = tree_height(root->left);
+        r_h = tree_height(root->right);
+        if ((l_h + r_h) > diameter){
+            diameter = l_h + r_h;
+        }
+        printf("%d %d %d\n", root->val, l_h, r_h);
+        return (l_h > r_h) ? l_h + 1 : r_h + 1;
+    }
+}
+
+int diameterOfBinaryTree(struct TreeNode* root) {
+    diameter = 0;
+    printf("%d\n", tree_height(root));
+    return diameter;
+}
